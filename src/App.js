d4871ac75507b0e2a8b2480541ce69342d2a9675
cole92 <aleksandar9212@gmail.com>
2024-11-02 22:01:15 +0100

    Imported react component and make html structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <div className='container'>
+          <h1>E comerce</h1>
+          <hr/>
+          <div className='row'>
+            <div className='col-md-9'>
+              Filter goes here
+              <hr/>
+              Products goes here
+            </div>
+            <div className='col-md-3'>
+              Basket goes here
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
