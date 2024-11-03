4634efbfd656a820ff4bdd90f20437fe5ea1d282
cole92 <aleksandar9212@gmail.com>
2024-11-03 12:33:09 +0100

    added prices and Add to cart btn

--- src/components/Products.js
+++ src/components/Products.js
@@ -5,7 +5,7 @@ export default class Products extends Component {
     const productItems = this.props.products.map(product => {
       return (
 
-        <div className='col-md-4'>
+        <div className='col-md-3 my-2 mx-4 p-2 border border-primary border-2 rounded'>
           <div className='thumbnail text-center'>
             <a href={`#${product.id}`}>
               <img src={`/products/${product.sku}_2.jpg`} alt={product.title} />
@@ -13,6 +13,10 @@ export default class Products extends Component {
                 {product.title}
               </div>
             </a>
+            <div>
+              <strong>Price: {product.price}</strong> <br/>
+              <button className='btn btn-primary'>Add to cart</button>
+            </div>
           </div>
         </div>
       )
